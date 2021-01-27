3589448c61d378090f132e60594d3a44ae19913b
ZapAnt <antoniozappone72@gmail.com>
2021-01-27 15:57:11 +0100

    synopsys for abs version

---
+++ syn_abs/init_syn_abs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SET_FILE=.synopsys_dc.setup
+if [ ! -f "./$SET_FILE" ]; then
+	echo "$SET_FILE is not here!"
+	exit
+fi
+
+rm -rf ./work/*
+rm -f ./command.log
+rm -f ./default.svf
+rm -f ./elaborate.txt
+rm -f ./filenames.log
+rm -f ../netlist_abs/RISCV_pipeline_abs.sdf
+rm -f ../netlist_abs/RISCV_pipeline_abs.v
+rm -f ../netlist_abs/RISCV_pipeline_abs.sdc
+
+source /software/scripts/init_synopsys_64.18
+dc_shell-xg-t -f ./syn_commands_abs.txt
+# design_vision &
