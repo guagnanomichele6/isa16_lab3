a56520b9d259d3e657da6883cb303a3256e95051
ZapAnt <74555174+ZapAnt@users.noreply.github.com>
2021-02-20 14:31:59 +0100

    clean repo, script relative paths, removed alu vhdl comments

--- abs/sim/work/tb_riscv_abs/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity tb_riscv_abs is
-end tb_riscv_abs;
--- no_abs/sim/work/tb_riscv/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity tb_riscv is
-end tb_riscv;
